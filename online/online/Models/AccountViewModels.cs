d2ac4a7103ddf4842e41c1962a1c3a73586ca59b
Qura-tul-ain <quratulaink960@gmail.com>
2018-11-18 17:13:28 +0500

    register

--- online/online/Models/AccountViewModels.cs
+++ online/online/Models/AccountViewModels.cs
@@ -65,9 +65,18 @@ namespace online.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "YourName")]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+       
+        [Display(Name = "CNIC")]
+        public string Cnic { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
@@ -79,6 +88,10 @@ namespace online.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public string Adress { get; set; }
+
+
     }
 
     public class ResetPasswordViewModel
