e01ed940eb9b8bd346a8ec0d023f3e1ceb4df636
AqsaZahid256 <aqsazahid938@gmail.com>
2018-11-23 18:36:39 +0500

    add

---
+++ Backend-master/online/online/Controllers/onlineDbEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace online.Controllers
+{
+	internal class onlineDbEntities
+	{
+		public object Products { get; internal set; }
+
+		internal void SaveChanges()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
