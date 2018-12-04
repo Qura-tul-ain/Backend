a421349b0d6f51a3b38251a60af4b8b5b3dab1a2
Qura-tul-ain <quratulaink960@gmail.com>
2018-12-04 21:11:29 +0500

    admin display

---
+++ online/online/Controllers/AdminController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using online;
+using online.Models;
+namespace online.Controllers
+{
+    public class AdminController : Controller
+    {
+        private onlineEntities2 db = new onlineEntities2();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            //onlineEntities2 db = new onlineEntities2();
+            //List<AspNetUser> lists = new List<AspNetUser>();
+            //lists = db.AspNetUsers.ToList();
+            return View();
+            //List<AspNetUser> lists = new List<AspNetUser>();
+            //List<RegisterViewModel> finalData=new List<RegisterViewModel>(); 
+            //lists = db.AspNetUsers.ToList();
+            //foreach (AspNetUser a in lists) {
+            //    RegisterViewModel temp = new RegisterViewModel();
+            //    temp.Username = a.UserName;
+            //    temp.Email = a.Email;
+            //    temp.Cnic = a.Cnic;
+            //    temp.Adress = a.Adress;
+            //    finalData.Add(temp);
+            //    }
+            //ViewBag.data = finalData;
+            //return View(finalData);
+            //return View(db.AspNetUsers.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ImageId,Images,ProductName,ProductDescp,BidAmount,AuctionDate,Category")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ImageId,Images,ProductName,ProductDescp,BidAmount,AuctionDate,Category")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
